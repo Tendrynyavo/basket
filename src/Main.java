d2fb55c27402090773d1b51067ed37807694ec57
Tendrynyavo <Tendrirazafimiandrisoa@gmail.com>
2022-12-09 11:13:03 +0300

    clean

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,5 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Formulaire formulaire = Match.getFormulaire();
         formulaire.initFrame(new JFrame());
-        // Chrono chrono = new Chrono();
-        // chrono.start();
-        // Thread.sleep(2000);
-        // chrono.pause();
-        // System.out.println(chrono.getDureeSec());
-        // chrono.resume();
-        // Thread.sleep(2000);
-        // chrono.stop();
-        // System.out.println(chrono.getDureeSec());
     }
 }
